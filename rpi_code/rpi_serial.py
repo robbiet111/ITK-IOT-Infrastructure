9055d633679f1943f5e0615fcefbc1faaab1272f
2478625j <2478625j@student.gla.ac.uk>
2022-02-09 13:20:24 +0000

    Add Raspberry Pi scripts

---
+++ rpi_code/rpi_serial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import serial
+
+if __name__ == "__main__":
+        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+        ser.reset_input_buffer()
+
+        while True:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                line_segments = line.split()
+                if (line_segments[0]=="data:"):
+                    print(line_segments[1])
+                else:
+                    print(line)
