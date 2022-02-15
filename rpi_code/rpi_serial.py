bbf23663bc784c102b9f7a392608031d669c7f00
2478625j <2478625j@student.gla.ac.uk>
2022-02-15 11:59:44 +0000

    Upload 3 files in row with time stamps in data

--- rpi_code/rpi_serial.py
+++ rpi_code/rpi_serial.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
 import serial
 from datetime import datetime
+import time
 from sharepoint import SharePoint
 
 if __name__ == "__main__":
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.reset_input_buffer()
 
-    time = datetime.now()
-    filename = "data_from_"+time.strftime("%Y%m%d")+".txt"
-    print(filename)
-    counter = 10
-    with open(filename, "w") as file:
-        while counter > 0:
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8').rstrip()
-                line_segments = line.split()
-                if (len(line_segments) > 0):
-                    if (line_segments[0]=="data:"):
-                        print(line_segments[1])
-                        file.write(line_segments[1]+"\n")
-                        counter = counter - 1
+    for i in range(3):
+        curr_time = datetime.now()
+        filename = "data_from_"+curr_time.strftime("%Y%m%d_%H%M%S")+".txt"
+        print(filename)
+        counter = 10
+        with open(filename, "w") as file:
+            while counter > 0:
+                if ser.in_waiting > 0:
+                    line = ser.readline().decode('utf-8').rstrip()
+                    line_segments = line.split()
+                    if (len(line_segments) > 0):
+                        if (line_segments[0]=="data:"):
+                            print(line_segments[1])
+                            file.write(datetime.now().strftime("%H%M%S")+": "+line_segments[1]+"\n")
+                            time.sleep(1)
+                            counter = counter - 1
+                        else:
+                            print(line)
                     else:
                         print(line)
-                else:
-                    print(line)
-    folder_name = 'From_serial'
+        folder_name = 'From_serial'
 
-    SharePoint().upload_file(filename, filename, folder_name)
+        SharePoint().upload_file(filename, filename, folder_name)
