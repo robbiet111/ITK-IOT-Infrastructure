992d78c0f86237869378b9338248503aa376185b
2478625j <2478625j@student.gla.ac.uk>
2022-02-15 11:26:49 +0000

    Upload a text file with 10 measurements to sharepoint

--- rpi_code/rpi_serial.py
+++ rpi_code/rpi_serial.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
 import serial
+from datetime import datetime
+from sharepoint import SharePoint
 
 if __name__ == "__main__":
-        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-        ser.reset_input_buffer()
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    ser.reset_input_buffer()
 
-        while True:
+    time = datetime.now()
+    filename = "data_from_"+time.strftime("%Y%m%d")+".txt"
+    print(filename)
+    counter = 10
+    with open(filename, "w") as file:
+        while counter > 0:
             if ser.in_waiting > 0:
                 line = ser.readline().decode('utf-8').rstrip()
                 line_segments = line.split()
                 if (len(line_segments) > 0):
                     if (line_segments[0]=="data:"):
                         print(line_segments[1])
+                        file.write(line_segments[1]+"\n")
+                        counter = counter - 1
                     else:
                         print(line)
                 else:
                     print(line)
+    folder_name = 'From_serial'
+
+    SharePoint().upload_file(filename, filename, folder_name)
