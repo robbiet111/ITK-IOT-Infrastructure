0d1e8e16aef68e6cf611fef2b06107943fa3341b
2478625j <2478625j@student.gla.ac.uk>
2022-02-16 14:36:13 +0000

    Read port from cmd

--- rpi_code/rpi_serial.py
+++ rpi_code/rpi_serial.py
@@ -3,15 +3,23 @@ import serial
 from datetime import datetime
 import time
 from sharepoint import SharePoint
+import argparse
 
 if __name__ == "__main__":
+    upload_port = '/dev/ttyUSB0'
+    parser=argparse.ArgumentParser(description="RPi Serial")
+    
+    parser.add_argument('--port',nargs='?', help='Specify port to be monitored')
+    args=parser.parse_args()
+    print(args)
+       
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.reset_input_buffer()
 
     for i in range(3):
         curr_time = datetime.now()
-        filename = "data_from_"+curr_time.strftime("%Y%m%d_%H%M%S")+".txt"
-        print(filename)
+        filename = "MAX31865_"+curr_time.strftime("%Y%m%d_%H%M%S")+".txt"
+        print("Data stored in:\n"+filename)
         counter = 10
         with open(filename, "w") as file:
             while counter > 0:
@@ -20,7 +28,6 @@ if __name__ == "__main__":
                     line_segments = line.split()
                     if (len(line_segments) > 0):
                         if (line_segments[0]=="data:"):
-                            print(line_segments[1])
                             file.write(datetime.now().strftime("%H%M%S")+": "+line_segments[1]+"\n")
                             time.sleep(1)
                             counter = counter - 1
@@ -29,5 +36,5 @@ if __name__ == "__main__":
                     else:
                         print(line)
         folder_name = 'From_serial'
-
+    
         SharePoint().upload_file(filename, filename, folder_name)
