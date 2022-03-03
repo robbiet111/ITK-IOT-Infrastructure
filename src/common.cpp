14b92323c8ec887d9b36b6ce287def693de7b845
2478625j <2478625j@student.gla.ac.uk>
2022-03-03 20:14:28 +0000

    Introduce common namespace. Part of https://stgit.dcs.gla.ac.uk/team-project-h/2021/cs34/cs34-main/-/issues/85
    This namespace contains variables and functions used by other namespaces.

---
+++ src/common.cpp
@@ -0,0 +1,57 @@
+#include "common_namespace.h"
+
+namespace common {
+    String influxdb_url;
+    String wifi_ssid;
+    String wifi_pass;
+    running_mode current_mode = measure;
+    bool refreshScreen = true;
+    int btnClick = true;
+    TFT_eSPI tft = TFT_eSPI(135, 240);
+    WiFiMulti wifimulti;
+    WebServer server(80);
+    void sensor_setup() {
+        Serial.println(wifi_ssid+" "+wifi_pass);
+        common::wifimulti.addAP(wifi_ssid.c_str(), wifi_pass.c_str());
+        delay(200);
+        Serial.println("");
+        Serial.print("Connecting to WiFi: ");
+        Serial.println(wifi_ssid);
+
+
+        common::clear_display(2);
+        tft.drawString("Connecting to WiFi", tft.width() / 2, tft.height() / 2 - 16);
+        tft.drawString(String(wifi_ssid), tft.width() / 2, tft.height() / 2 + 16);
+
+        int counter = 0;
+
+        while (common::wifimulti.run() != WL_CONNECTED) {
+            delay(100);
+            counter += 1;
+            if (counter == 5) {
+            Serial.println("Entering CLI");
+            current_mode = enter_cli;
+            common::clear_display(2);
+            tft.drawString("Connection failed", tft.width() / 2, tft.height() / 2-16);
+            tft.drawString("Entering CLI", tft.width() / 2, tft.height() / 2+16);
+            delay(2500);
+            break;
+            }
+        }
+        if (current_mode == measure) {
+            WiFi.setHostname(ESP32_HOST_NAME);
+            Serial.print("WiFi connected: "); Serial.println(WiFi.SSID());
+            Serial.print("Hostname: ");       Serial.println(WiFi.getHostname());
+            Serial.print("IP address: ");     Serial.println(WiFi.localIP()); char buf[16]; sprintf(buf, "%d.%d.%d.%d", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3] );
+            Serial.print("MAC address: ");    Serial.println(WiFi.macAddress());
+
+            common::clear_display(1);
+            tft.drawString("WiFi connected: " + String(WiFi.SSID()), tft.width() / 2, tft.height() / 2 - 24);
+            tft.drawString("Hostname: " + String(WiFi.getHostname()), tft.width() / 2, tft.height() / 2 - 8);
+            tft.drawString("IP address: " + String(buf), tft.width() / 2, tft.height() / 2 + 8);
+            tft.drawString("MAC address: " + String(WiFi.macAddress()), tft.width() / 2, tft.height() / 2 + 24);
+            delay(5000);
+            tft.fillScreen(TFT_BLACK);
+        }
+    }
+}
