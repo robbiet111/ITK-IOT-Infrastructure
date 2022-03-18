4ae064e7390778fab0bd8248dcf1d05de9f8ab8b
2478625j <2478625j@student.gla.ac.uk>
2022-03-18 09:51:07 +0000

    Create template branch with begin and measure experiment function empty

--- include/experiment_namespace.h
+++ include/experiment_namespace.h
@@ -6,24 +6,18 @@
 
   Change log:
   Version: 1.0.0 [18/03/2022]
-  First full release.
+  Template for further development.
 
   This code is licensed under MIT license (see LICENSE for details)
 ***/
 
-#include <Adafruit_MAX31865.h>
 #include "common_namespace.h"
 
-// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
-#define RREF 430.0
-// The 'nominal' 0-degrees-C resistance of the sensor
-// 100.0 for PT100, 1000.0 for PT1000
-#define RNOMINAL 100.0
-
 namespace experiment {
-    extern Adafruit_MAX31865 max_ada;
+    // Declare your experiment namespace variables as extern here
+
     inline void begin() {
-        max_ada.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary 
+      // Fill in your begin function here
     }
     void measure();
 }
--- src/experiment.cpp
+++ src/experiment.cpp
@@ -6,7 +6,7 @@
 
   Change log:
   Version: 1.0.0 [18/03/2022]
-  First full release.
+  Template for further development.
 
   This code is licensed under MIT license (see LICENSE for details)
 ***/
@@ -16,50 +16,9 @@
 using namespace common;
 
 namespace experiment {
-    Adafruit_MAX31865 max_ada = Adafruit_MAX31865(33, 25, 26, 27);
+    // Define any variables in the experiment namespace here
 
     void measure() {
-        static uint64_t timeStamp = 0;
-        if (millis() - timeStamp > 1000) { // measure data every 1 seconds
-            timeStamp = millis();
-
-        uint16_t rtd = max_ada.readRTD();
-        float ratio = rtd;
-        ratio /= 32768;
-        float temperature = 0.0;
-        temperature = max_ada.temperature(RNOMINAL, RREF);
-        
-        // Check and print any faults
-        uint8_t fault = max_ada.readFault();
-        if (fault) {
-        Serial.print("Fault 0x"); Serial.println(fault, HEX);
-        if (fault & MAX31865_FAULT_HIGHTHRESH) {
-            Serial.println("RTD High Threshold"); 
-        }
-        if (fault & MAX31865_FAULT_LOWTHRESH) {
-            Serial.println("RTD Low Threshold"); 
-        }
-        if (fault & MAX31865_FAULT_REFINLOW) {
-            Serial.println("REFIN- > 0.85 x Bias"); 
-        }
-        if (fault & MAX31865_FAULT_REFINHIGH) {
-            Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
-        }
-        if (fault & MAX31865_FAULT_RTDINLOW) {
-            Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
-        }
-        if (fault & MAX31865_FAULT_OVUV) {
-            Serial.println("Under/Over voltage"); 
-        }
-        max_ada.clearFault();
-        }
-            else {
-                Serial.print("data: "); Serial.print(temperature); Serial.println(" °C");
-
-                clear_display(2);
-                put_on_display(String("T: ") + String(temperature) + " *C", 0);
-
-            }		
-        }	  
+        // Fill in the body of your measurement function here
     }
 }
